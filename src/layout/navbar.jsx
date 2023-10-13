3114d5810e15ede1b1f114f3f1d04d6fa672b19e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-13 13:09:39 +0330

    Subject: Navbar, Description: Add content to navbar, Date: 10/13/2023

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import Logo from '../assets/logo.svg'
 
 export default function Navbar() {
-  return (
-    <div className='max-w-[1220px] mx-auto bg-white'>Navbar</div>
-  )
+  const menus = [
+    {
+      title: "صفحه اصلی",
+      to: "",
+    },
+    {
+      title: "خرید و فروش ارز دیجیتال",
+      to: "",
+    },
+    {
+      title: "قیمت‌های آنلاین",
+      to: "",
+    },
+    {
+      title: "مقالات آموزشی",
+      to: "",
+    },
+    {
+      title: "وبلاگ",
+      to: "",
+    },
+    {
+      title: "ارتباط با ما",
+      to: "",
+    },
+  ];
+
+  return <div className="max-w-[1220px] mx-auto bg-black py-4 px-8 mt-4 rounded-xl">
+    <div className="logo">
+        <img src={Logo} alt="logo" />
+    </div>
+    <div className="flex items-center gap-4">
+        {menus.map(({title, to})=> <Link to={to}>{title}</Link>)}
+    </div>
+  </div>;
 }
