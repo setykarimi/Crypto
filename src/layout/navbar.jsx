10e60faf39ff65a8f6a94ae83ad1922cb5b8dfed
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-14 23:24:05 +0330

    Subject: Style, Description: Edit content, Date: 10/14/2023

--- src/layout/navbar.jsx
+++ src/layout/navbar.jsx
@@ -1,40 +1,69 @@
 import React from "react";
-import Logo from '../assets/logo.svg'
+import { Link, NavLink } from "react-router-dom";
+import Logo from "../assets/logo.svg";
 
 export default function Navbar() {
   const menus = [
     {
       title: "صفحه اصلی",
-      to: "",
+      to: "/",
     },
     {
       title: "خرید و فروش ارز دیجیتال",
-      to: "",
+      to: "/example1",
     },
     {
       title: "قیمت‌های آنلاین",
-      to: "",
+      to: "/example2",
     },
     {
       title: "مقالات آموزشی",
-      to: "",
+      to: "/example3",
     },
     {
       title: "وبلاگ",
-      to: "",
+      to: "/example4",
     },
     {
       title: "ارتباط با ما",
-      to: "",
+      to: "/example5",
     },
   ];
 
-  return <div className="max-w-[1220px] mx-auto bg-black py-4 px-8 mt-4 rounded-xl">
-    <div className="logo">
+  return (
+    <div className="max-w-[1220px] mx-auto py-4 px-8 mt-4 rounded-2xl flex gap-8 items-center bg-white">
+      <div className="logo">
         <img src={Logo} alt="logo" />
+      </div>
+      <div className="flex items-center gap-4">
+        {menus.map(({ title, to }) => (
+          <NavLink
+            className={({ isActive }) =>
+              isActive
+                ? "text-blue-primary border-b-2 border-b-blue-primary font-bold pb-1"
+                : "text-gray-300 text-14 pb-1"
+            }
+            to={to}
+          >
+            {title}
+          </NavLink>
+        ))}
+      </div>
+
+      <div className="flex gap-4 mr-auto">
+        <Link
+          to=""
+          className="text-14 text-white bg-blue-primary py-2 px-4 rounded-lg"
+        >
+          ورود / ثبت نام
+        </Link>
+        <Link
+          to=""
+          className="text-14 text-white bg-red-primary py-2 px-4 rounded-lg"
+        >
+          پشتیبانی
+        </Link>
+      </div>
     </div>
-    <div className="flex items-center gap-4">
-        {menus.map(({title, to})=> <Link to={to}>{title}</Link>)}
-    </div>
-  </div>;
+  );
 }
