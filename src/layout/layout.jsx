e2b93ec59f5b551bc33f618df1cc3567a10f37e6
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-14 23:23:55 +0330

    Subject: Style, Description: Edit bg color, Date: 10/14/2023

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -4,7 +4,7 @@ import Navbar from './navbar'
 
 export default function Layout({children}) {
   return (
-    <div className='container'>
+    <div className='md:container bg-blue-primary'>
         <Navbar />
         {children}
         <Footer />
