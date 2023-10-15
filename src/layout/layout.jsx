3b276aca0f9f6f9238bb77a23ae3a26cd1ffb889
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-15 22:53:55 +0330

    Subject: Crypto, Description: Add banner, Date: 10/15/2023

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Footer from './footer'
 import Navbar from './navbar'
+import BannerHome from '../components/banner/bannerHome'
 
 export default function Layout({children}) {
   return (
-    <div className='md:container bg-blue-primary'>
+    <div className='md:container'>
         <Navbar />
+        <BannerHome />
         {children}
         <Footer />
     </div>
