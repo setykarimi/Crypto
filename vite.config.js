9f67b98037bfa2951547c3e1ab5f50b46e5df54e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-17 22:08:02 +0330

    Subject: Config, Description: Add new alias, Date: 10/17/2023

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,10 @@ export default defineConfig({
   resolve: {
     alias: {
       components: "/src/components",
-      images:  "/src/assets/images/",
+      images: "/src/assets/images/",
+      pages: "/src/pages/",
+      layout: "/src/layout/",
+      routes: "/src/routes/",
     },
   },
-})
+});
