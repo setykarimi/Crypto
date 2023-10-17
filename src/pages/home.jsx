29f170f1e55a5c3a29047e0bc0c83b23021dacec
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-17 22:07:30 +0330

    Subject: Pages, Description: Create new folder for PagesDate: 10/17/2023

---
+++ src/pages/home.jsx
@@ -0,0 +1,10 @@
+import BannerHome from "components/banner/bannerHome";
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <>
+      <BannerHome />
+    </>
+  );
+}
