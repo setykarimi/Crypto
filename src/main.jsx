893d73a3461fdcd5261f296ca25b70d739754ade
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-17 22:07:07 +0330

    Subject: Routes, Description: Create new folder for routes, Date: 10/17/2023

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom'
+import { routes } from 'routes/routes.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <RouterProvider router={routes} />,
 )
