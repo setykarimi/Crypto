da5eb4510646b06c8e9936ebd2ba01e0f9c1c112
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-19 10:28:30 +0330

    Subject: Home page, Description: Edit title content, Date: 10/19/2023

--- src/components/common/titles/titleSections.jsx
+++ src/components/common/titles/titleSections.jsx
@@ -1,7 +1,18 @@
-import React from 'react'
-
-export default function SectionsTitle() {
+export default function SectionsTitle(props) {
+  const { number, title, subTitle, children } = props;
   return (
-    <div>SectionsTitle</div>
-  )
+    <div className="mb-8 flex justify-between">
+      <div className="font-extraBold relative ml-auto">
+        <h2 className="text-red-primary md:text-4xl text-2xl z-1 relative mr-6">
+          {title}
+        </h2>
+        <span className="text-blue-100 text-7xl absolute top-0 right-0 z-0">
+          {number}
+        </span>
+        <span className="text-gray-800 relative -mt-2 block">{subTitle}</span>
+      </div>
+
+      {children}
+    </div>
+  );
 }
