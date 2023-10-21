358cc74b6cf579f767778f00e23a59ef621b247c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-21 22:04:58 +0330

    Subject: Home page, Description: Edit for responsive view, Date: 10/21/2023

--- src/components/common/cards/cardCryptoMarket.jsx
+++ src/components/common/cards/cardCryptoMarket.jsx
@@ -5,7 +5,7 @@ import React from "react";
 export default function CryptoMarketCards() {
   return (
     <div
-      className="lg:mt-24 mt-12 pb-24"
+      className="lg:mt-24 mt-12 pb-12"
       style={{
         background: `url(${cryptoMarketBg}) no-repeat center right`,
         backgroundSize: "contain",
