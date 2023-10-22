4e29f877e82872096eb4a7c2f95be1d1095e5f8f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-22 21:22:48 +0330

    Subject: Caculator, Description: Add Calculator component, Date: 10/22/2023

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,6 +2,7 @@ import CryptoMarketCards from "common/cards/cardCryptoMarket";
 import OnlinePricesTable from "common/table/tableOnlinePrices";
 import HomeBanner from "pagesComp/home/banner/bannerHome";
 import BannerWhyCrypto from "pagesComp/home/banner/bannerWhyCrypto";
+import OnlineCalculator from "pagesComp/home/calculator/onlineCalculator";
 
 export default function HomePage() {
   return (
@@ -10,6 +11,7 @@ export default function HomePage() {
       <CryptoMarketCards />
       <OnlinePricesTable />
       <BannerWhyCrypto />
+      <OnlineCalculator />
     </>
   );
 }
