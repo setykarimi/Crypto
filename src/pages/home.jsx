67db3ee7067ea34ee0bb863c274151c4ce4d2f9e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-25 21:23:10 +0330

    Subject: Home page, Description: Add warning component, Date: 10/25/2023

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -6,6 +6,7 @@ import BannerWhyCrypto from "pagesComp/home/banner/bannerWhyCrypto";
 import OnlineCalculator from "pagesComp/home/calculator/onlineCalculator";
 import CryptoContent from "pagesComp/home/content/cryptoContent";
 import DigitalWallet from "pagesComp/home/content/digitalWallet";
+import Warning from "pagesComp/home/content/warning";
 
 export default function HomePage() {
   return (
@@ -18,6 +19,7 @@ export default function HomePage() {
       <CryptoContent />
       <Blogs />
       <DigitalWallet />
+      <Warning />
     </>
   );
 }
