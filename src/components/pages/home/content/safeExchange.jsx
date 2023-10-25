f22325f471ad44b94c47019bae95f2f4353cea54
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-25 21:47:56 +0330

    Subject: Home page, Description: Create new component safeExchange, Date: 10/25/2023

---
+++ src/components/pages/home/content/safeExchange.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { BsArrowLeft } from "react-icons/bs";
+
+export default function SafeExchange() {
+  return (
+    <div className="bg-orange-primary lg:mt-24 mt-12 py-8 flex flex-col">
+      <h4 className="font-extraBold text-4xl text-center text-white">
+        با بهترین قیمت از امن‌ترین صرافی خرید کنید
+      </h4>
+      <button className="flex items-center gap-2 border border-white text-white rounded-lg py-2 px-4 mx-auto mt-6">
+        همین حالا ثبت‌نام کن
+        <BsArrowLeft />
+      </button>
+    </div>
+  );
+}
