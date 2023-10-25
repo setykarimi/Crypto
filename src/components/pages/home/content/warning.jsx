63d9e2b780525d8c730dcad730b0214a5abdb091
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-25 21:21:34 +0330

    Subject: Home page, Description: Create new component for warning, Date: 10/25/2023

---
+++ src/components/pages/home/content/warning.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Warning() {
+  return (
+    <div className="bg-red-500">
+      <section className="inner-container mx-auto md:px-0 px-2">
+        <p className="text-justify text-white font-bold py-4 leading-loose">
+          خواهشمند است در هنگام خرید و فروش ارزهای دیجیتال، به اندازه کافی
+          مطالعه و دقت نموده و بر اساس دانش خود اقدام به مبادله نمایید. اکسچنج
+          کریپتر بعنوان پلتفورم هوشمند خرید و فروش ارزهای دیجیتال، به هیچ وجه به
+          کاربران خود برای خرید، فروش و یا سرمایه گذاری، توصیه ای ننموده و همه
+          مبادلات با تصمیم و مسئولیت خود کاربران صورت می پذیرد.
+        </p>
+      </section>
+    </div>
+  );
+}
