0726918980e4418a576fdb83a9e058551840ac42
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-26 12:01:21 +0330

    Subject: Footer, Description: Edit for footer, Date: 10/26/2023

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import React from "react";
+import footerBg from "images/backgrounds/footerBg.svg";
+import logoRed from "images/logoRed.svg";
+import logoWhite from "images/logoWhite.svg";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <footer
+      className="inner-container mx-auto mt-8 rounded-2xl bg-blue-primary p-8 text-white"
+      style={{ backgroundImage: `url(${footerBg})` }}
+    >
+      <div className="grid md:grid-cols-2 grid-cols-1 gap-8">
+        <div>
+          <img src={logoRed} alt="logo" className="mx-auto" />
+          <span className="block mt-4">
+            عضویت در خبرنامه کریپتر آخرین اخبار و اطلاعیه‌ها را در ایمیل خود
+            دریافت کنید:
+          </span>
+        </div>
+        <div>
+          
+        </div>
+        <div></div>
+      </div>
+      <div className="mt-8">
+        <img src={logoWhite} alt="logo" className="w-20" />
+
+        <p className="mt-2 text-justify leading-relaxed">
+          در صرافی ارز دیجیتال کریپتر که امن ترین صرافی کریپتوکارنسی
+          (Cryptocurrency) کشور است، شما توانایی خرید و فروش انواع ارزهای
+          دیجیتال معتبر دنیا مانند بیت کوین، اتریوم، تتر، دوج کوین، شیبا و ...
+          را به صورت حرفه ای و سریع را خواهید داشت.با اطمینان خاطر فراوان در
+          صرافی امن کریپتر اقدام به معامله و خرید ارز دیجیتال نمایید. پلتفرم
+          ترید حرفه ای
+        </p>
+      </div>
+    </footer>
+  );
 }
