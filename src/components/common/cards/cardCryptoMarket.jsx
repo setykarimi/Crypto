7971ae40042c641d1078ed80f2222a42a4727740
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-27 10:30:23 +0330

    Subject: CryptoMarket, Description: Add skeleton for showing data, Date: 10/27/2023

--- src/components/common/cards/cardCryptoMarket.jsx
+++ src/components/common/cards/cardCryptoMarket.jsx
@@ -1,6 +1,8 @@
 import SectionsTitle from "common/titles/titleSections";
+import { useCoins } from "hooks/useCoins";
 import cryptoMarketBg from "images/backgrounds/cryptoMarketBg.svg";
 import React from "react";
+import { numberSeprator } from "utils/numberSeprator";
 
 export default function CryptoMarketCards() {
   return (
@@ -16,7 +18,7 @@ export default function CryptoMarketCards() {
         <SectionsTitle number={1} title="کریپتر" subTitle="بازارهای">
           <button className="bg-blue-primary rounded-lg flex items-center gap-2 p-2 h-fit md:text-sm text-xs font-semiBold">
             <span className="bg-white text-blue-700 px-2 py-1 rounded-md">
-              ریال IR
+            دلار $
             </span>
             <span className="text-white">تتر USDT</span>
           </button>
@@ -25,10 +27,7 @@ export default function CryptoMarketCards() {
 
         {/* Cards */}
         <section className="grid md:grid-cols-4 grid-cols-2 xl:gap-12 lg:gap-8 gap-4">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          <Cards />
         </section>
         {/* End Card Section */}
       </section>
@@ -36,20 +35,60 @@ export default function CryptoMarketCards() {
   );
 }
 
-const Card = () => {
+const Cards = () => {
+  const { data: coins, isLoading, error } = useCoins("/coins?&limit=4&page=2");
+
+  if (isLoading) {
+    return (
+      <>
+        <LoadingCard />
+        <LoadingCard />
+        <LoadingCard />
+        <LoadingCard />
+      </>
+    );
+  }
+
+  return coins?.result?.map((coin) => (
+    <Card
+      icon={coin.icon}
+      name={coin.name}
+      price={numberSeprator(Math.round(coin.price))}
+      priceChange1h={coin.priceChange1h}
+      priceChange1d={coin.priceChange1d}
+    />
+  ));
+};
+
+const Card = (props) => {
+  const { icon, name, price, priceChange1h, priceChange1d } = props;
   return (
     <div className="bg-blue-primary lg:p-8 p-4 lg:rounded-[3rem] rounded-3xl flex flex-col items-center lg:gap-4 gap-2">
-      <div className="lg:h-24 h-16 lg:w-24 w-16 rounded-full bg-white"></div>
-      <div className="text-orange-primary flex gap-4 font-bold text-lg">
-        <span className="blocl">کاردانو</span>
-        <span className="block">ADA</span>
-      </div>
+      <img
+        src={icon}
+        className="lg:h-24 h-16 lg:w-24 w-16 rounded-full"
+        alt="icon"
+      />
+      <span className="text-orange-primary flex gap-4 font-bold text-lg block">
+        {name}
+      </span>
       <span className="text-white block lg:text-xl text-lg lg:font-bold">
-        98,254 ریال
+        {price} دلار
       </span>
-      <span className="text-green-primary block lg:font-bold lg:text-base text-sm">
-        0.25 (150)
+      <span className="text-green-primary block lg:font-bold lg:text-base text-sm ltr">
+        {priceChange1h} ({priceChange1d})
       </span>
+    </div>
+  );
+};
+
+const LoadingCard = () => {
+  return (
+    <div className="bg-blue-primary lg:p-8 p-4 lg:rounded-[3rem] rounded-3xl flex flex-col items-center lg:gap-8 gap-4">
+      <div className="lg:h-24 h-16 lg:w-24 w-16 rounded-full bg-gray-100" />
+      <span className="block bg-gray-100 h-2 w-2/3 rounded-lg"></span>
+      <span className="block bg-gray-100 h-2 w-2/3 rounded-lg"></span>
+      <span className="block bg-gray-100 h-2 w-2/3 rounded-lg"></span>
     </div>
   );
 };
