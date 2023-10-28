3023e4f44a31a53890abf58167e6712f0a812c4e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-28 21:57:45 +0330

    Subject: News, Description: Create new component for page title, Date: 10/28/2023

---
+++ src/components/common/titles/titlePage.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function TitlePage() {
+  return (
+    <div>
+        <span className='font-extraBold text-red-primary text-[4rem] block mr-8'>کـــــــریـپـتـر</span>
+        <span className='font-extraBold text-blue-primary text-[2rem] block -mt-8'>آخرین اخبار</span>
+    </div>
+  )
+}
