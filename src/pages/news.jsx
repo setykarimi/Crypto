21ea0f153a687e25594d927889940c41a5f8903e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-28 21:31:44 +0330

    Subject: News, Description: Create news page, Date: 10/28/2023

---
+++ src/pages/news.jsx
@@ -0,0 +1,10 @@
+import BannerNews from "pagesComp/news/banner/bannerNews";
+import React from "react";
+
+export default function NewsPage() {
+  return (
+    <>
+      <BannerNews />
+    </>
+  );
+}
