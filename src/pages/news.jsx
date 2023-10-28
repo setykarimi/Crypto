9c1c0bfbaac19aaa3fd03deb51d0778850101698
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-28 21:57:02 +0330

    Subject: News, Description: Add new component to news page, Date: 10/28/2023

--- src/pages/news.jsx
+++ src/pages/news.jsx
@@ -1,10 +1,16 @@
+import TitlePage from "common/titles/titlePage";
 import BannerNews from "pagesComp/news/banner/bannerNews";
+import InputFilter from "pagesComp/news/input/inputFilter";
 import React from "react";
 
 export default function NewsPage() {
   return (
     <>
       <BannerNews />
+      <section className="inner-container mx-auto px-0 grid md:grid-cols-2 gap-8 mt-20 items-center">
+        <TitlePage />
+        <InputFilter />
+      </section>
     </>
   );
 }
