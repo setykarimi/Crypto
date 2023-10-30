1ba4966c5e26ef9104933614ef7d838b2f704ddb
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-10-30 22:39:50 +0330

    Subject: Config, Description: Create new file for clean imports, Date: 10/30/2023

---
+++ src/assets/images/index.js
@@ -0,0 +1,19 @@
+import news1_img from '@/assets/images/news/news1.png'
+import news2_img from '@/assets/images/news/news2.png'
+import news3_img from '@/assets/images/news/news3.png'
+import news4_img from '@/assets/images/news/news4.png'
+import news5_img from '@/assets/images/news/news5.png'
+import news6_img from '@/assets/images/news/news6.png'
+import news7_img from '@/assets/images/news/news7.png'
+import news8_img from '@/assets/images/news/news8.png'
+
+export {
+    news1_img,
+    news2_img,
+    news3_img,
+    news4_img,
+    news5_img,
+    news6_img,
+    news7_img,
+    news8_img,
+}
