1ede99c8d3ea13d4456e0687ea30136bd879a939
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-02 13:25:14 +0330

    Subject: Config, Description: Edit for importing component, Date: 11/02/2023

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,19 +1,70 @@
-import news1_img from '@/assets/images/news/news1.png'
-import news2_img from '@/assets/images/news/news2.png'
-import news3_img from '@/assets/images/news/news3.png'
-import news4_img from '@/assets/images/news/news4.png'
-import news5_img from '@/assets/images/news/news5.png'
-import news6_img from '@/assets/images/news/news6.png'
-import news7_img from '@/assets/images/news/news7.png'
-import news8_img from '@/assets/images/news/news8.png'
+// @@@__________ news __________@@@
+import news1_img from "@assets/images/news/news1.png";
+import news2_img from "@assets/images/news/news2.png";
+import news3_img from "@assets/images/news/news3.png";
+import news4_img from "@assets/images/news/news4.png";
+import news5_img from "@assets/images/news/news5.png";
+import news6_img from "@assets/images/news/news6.png";
+import news7_img from "@assets/images/news/news7.png";
+import news8_img from "@assets/images/news/news8.png";
+
+// @@@__________ logos __________@@@
+import logo_red from "@assets/images/logos/logoRed.svg";
+import logo_main from "@assets/images/logos/logo.svg";
+import logo_gray from "@assets/images/logos/logoGray.svg";
+import logo_white from "@assets/images/logos/logoWhite.svg";
+
+// @@@__________ backgrounds __________@@@
+import footer_bg from "@assets/images/backgrounds/footerBg.svg";
+import crypto_market_bg from "@assets/images/backgrounds/cryptoMarketBg.svg";
+import online_prices_bg from "@assets/images/backgrounds/onlinePricesBg.png";
+import banner_home_bg from "@assets/images/banner/bannerHomeBg.svg";
+import online_calculator_bg from "@assets/images/backgrounds/onlineCalculatorBg.svg";
+
+// @@@__________ blog __________@@@
+import blog1_img from "@assets/images/blog/blog1.png";
+
+// @@@__________ items __________@@@
+import banner_home_wallet from "@assets/images/banner/bannerHomeWallet.svg";
+import credit_card from "@assets/images/items/creditCard.svg";
+import wallet from "@assets/images/items/wallet.svg";
+import digital_wallet_title from "@assets/images/items/digitalWalletTitle.svg";
+import new_banner_img from '@assets/images/items/newsBannerItem.svg'
+import arrow_down from "@assets/images/icon/arrowDown.svg";
+
 
 export {
-    news1_img,
-    news2_img,
-    news3_img,
-    news4_img,
-    news5_img,
-    news6_img,
-    news7_img,
-    news8_img,
-}
+  news1_img,
+  news2_img,
+  news3_img,
+  news4_img,
+  news5_img,
+  news6_img,
+  news7_img,
+  news8_img,
+
+  // @@@__________ logos __________@@@
+  logo_red,
+  logo_main,
+  logo_gray,
+  logo_white,
+
+  // @@@__________ backgrounds __________@@@
+  footer_bg,
+  crypto_market_bg,
+  online_prices_bg,
+  banner_home_bg,
+  online_calculator_bg,
+
+
+  // @@@__________ blog __________@@@
+  blog1_img,
+
+  // @@@__________ items __________@@@
+  banner_home_wallet,
+  credit_card,
+  wallet,
+  digital_wallet_title,
+  new_banner_img,
+  arrow_down
+};
