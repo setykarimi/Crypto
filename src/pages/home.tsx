aaf2cd870fc6c8e37f6aa9f3158f36ba25bc072b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-02 13:27:35 +0330

    Subject: Config, Description: Edit for importing component, Date: 11/02/2023

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,14 +1,14 @@
-import FrequentQuestions from "common/accordion/frequentQuestions";
-import Blogs from "common/blogs/blogs";
-import CryptoMarketCards from "common/cards/cardCryptoMarket";
-import OnlinePricesTable from "common/table/tableOnlinePrices";
-import HomeBanner from "pagesComp/home/banner/bannerHome";
-import BannerWhyCrypto from "pagesComp/home/banner/bannerWhyCrypto";
-import OnlineCalculator from "pagesComp/home/calculator/onlineCalculator";
-import CryptoContent from "pagesComp/home/content/cryptoContent";
-import DigitalWallet from "pagesComp/home/content/digitalWallet";
-import SafeExchange from "pagesComp/home/content/safeExchange";
-import Warning from "pagesComp/home/content/warning";
+import FrequentQuestions from "@common/accordion/frequentQuestions";
+import Blogs from "@common/blogs/blogs";
+import CryptoMarketCards from "@common/cards/cardCryptoMarket";
+import OnlinePricesTable from "@common/table/tableOnlinePrices";
+import HomeBanner from "@pagesComp/home/banner/bannerHome";
+import BannerWhyCrypto from "@pagesComp/home/banner/bannerWhyCrypto";
+import OnlineCalculator from "@pagesComp/home/calculator/onlineCalculator";
+import CryptoContent from "@pagesComp/home/content/cryptoContent";
+import DigitalWallet from "@pagesComp/home/content/digitalWallet";
+import SafeExchange from "@pagesComp/home/content/safeExchange";
+import Warning from "@pagesComp/home/content/warning";
 
 export default function HomePage() {
   return (
