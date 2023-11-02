1eb870300d4891029810d15ff76b2060d7e6363a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-02 13:27:06 +0330

    Subject: Config, Description: Edit for importing component, Date: 11/02/2023

--- src/components/pages/news/newsContent/news.tsx
+++ src/components/pages/news/newsContent/news.tsx
@@ -1,11 +1,30 @@
+import { static_data } from "@data";
 import NewsCard from "./newsCard";
-import { newsData } from "@/staticData";
+
+interface NewsType {
+  img: string;
+  title: string;
+  desc: string;
+  author: string;
+  date: string;
+  view: number;
+}
 
 export default function NewsContent() {
-
   return (
     <section className="inner-container mx-auto px-0 grid lg:grid-cols-2 grid-cols-1 xl:gap-12 gap-8 lg:mt-24 mt-12 pb-12">
-      {newsData.news.map(({img, title, desc, author, date, view})=> <NewsCard img={img} title={title} desc={desc} author={author} date={date} view={view}/>)}
+      {static_data.news_data.news.map(
+        ({ img, title, desc, author, date, view }: NewsType) => (
+          <NewsCard
+            img={img}
+            title={title}
+            desc={desc}
+            author={author}
+            date={date}
+            view={view}
+          />
+        )
+      )}
     </section>
   );
 }
