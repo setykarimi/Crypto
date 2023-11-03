e85513436c1cce4745b98fc3d1ed341bdb67883a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-03 20:14:20 +0330

    Subject: News, Description: Edit for filtering data, Date: 11/03/2023

--- src/pages/news.tsx
+++ src/pages/news.tsx
@@ -1,17 +1,25 @@
 import TitlePage from "@common/titles/titlePage";
+import { static_data } from "@data";
 import BannerNews from "@pagesComp/news/banner/bannerNews";
 import InputFilter from "@pagesComp/news/input/inputFilter";
 import NewsContent from "@pagesComp/news/newsContent/news";
+import { useState } from "react";
 
 export default function NewsPage() {
+  const [filteredNews, setFilteredNews] = useState<
+    [] | static_data.news_init_type.NewsInitType
+  >([]);
+
   return (
     <>
       <BannerNews />
       <section className="inner-container mx-auto px-0 grid md:grid-cols-2 gap-8 mt-20 items-center">
         <TitlePage />
-        <InputFilter />
+        <InputFilter setFilteredNews={setFilteredNews} />
       </section>
-      <NewsContent />
+      <NewsContent
+        news={filteredNews.length ? filteredNews : static_data.news_data.news}
+      />
     </>
   );
 }
