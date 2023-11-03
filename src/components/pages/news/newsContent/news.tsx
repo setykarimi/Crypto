59949ffacfd116ff1d56f52c03e545b8185c7a55
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-03 20:14:14 +0330

    Subject: News, Description: Edit for filtering data, Date: 11/03/2023

--- src/components/pages/news/newsContent/news.tsx
+++ src/components/pages/news/newsContent/news.tsx
@@ -1,4 +1,3 @@
-import { static_data } from "@data";
 import NewsCard from "./newsCard";
 
 interface NewsType {
@@ -10,21 +9,21 @@ interface NewsType {
   view: number;
 }
 
-export default function NewsContent() {
+export default function NewsContent({ news }: any) {
+  console.log(news);
+
   return (
     <section className="inner-container mx-auto px-0 grid lg:grid-cols-2 grid-cols-1 xl:gap-12 gap-8 lg:mt-24 mt-12 pb-12">
-      {static_data.news_data.news.map(
-        ({ img, title, desc, author, date, view }: NewsType) => (
-          <NewsCard
-            img={img}
-            title={title}
-            desc={desc}
-            author={author}
-            date={date}
-            view={view}
-          />
-        )
-      )}
+      {news.map(({ img, title, desc, author, date, view }: NewsType) => (
+        <NewsCard
+          img={img}
+          title={title}
+          desc={desc}
+          author={author}
+          date={date}
+          view={view}
+        />
+      ))}
     </section>
   );
 }
