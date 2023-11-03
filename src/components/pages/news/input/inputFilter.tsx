453dddb7ec56cd5c36f92e268cdecbb223458e32
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-03 20:13:55 +0330

    Subject: News, Description: Edit for filtering data, Date: 11/03/2023

--- src/components/pages/news/input/inputFilter.tsx
+++ src/components/pages/news/input/inputFilter.tsx
@@ -1,13 +1,31 @@
+import { static_data } from "@data";
 import { BsFilterLeft } from "react-icons/bs";
 import { RiSearch2Fill } from "react-icons/ri";
 
-export default function InputFilter() {
+interface InputFilterProps {
+  setFilteredNews: React.Dispatch<
+    React.SetStateAction<[] | static_data.news_init_type.NewsInitType>
+  >;
+}
+
+export default function InputFilter({ setFilteredNews }: InputFilterProps) {
+  const changeHandler = (e: React.FormEvent<HTMLInputElement>): void => {
+    const { value } = e.currentTarget;
+
+    const filteredNews = static_data.news_data.news.filter(({ title }) =>
+      title.includes(value)
+    );
+
+    setFilteredNews(filteredNews);
+  };
+
   return (
     <div className="bg-white shadow-md py-2 pr-4 pl-3 rounded-xl focus-within:shadow-lg transition-all flex items-center gap-2 md:w-3/4 mr-auto w-full">
       <RiSearch2Fill className="text-gray-100" size={25} />
       <input
         placeholder="اخبار رو اینجا پیدا کن ..."
         className="placeholder:text-gray-100 w-full py-2 outline-none placeholder:text-sm"
+        onChange={changeHandler}
       />
       <span className="block mr-auto bg-blue-primary p-1 rounded-lg text-white">
         <BsFilterLeft size={25} />
