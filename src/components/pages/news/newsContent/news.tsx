934420ada51b9eb7bc66dea29402e0843bcd5a40
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-04 21:45:47 +0330

    Subject: News, Description: Import type from external file, Date: 11/04/2023

--- src/components/pages/news/newsContent/news.tsx
+++ src/components/pages/news/newsContent/news.tsx
@@ -1,17 +1,7 @@
 import NewsCard from "./newsCard";
-
-interface NewsType {
-  img: string;
-  title: string;
-  desc: string;
-  author: string;
-  date: string;
-  view: number;
-}
+import { NewsType } from "./type";
 
 export default function NewsContent({ news }: any) {
-  console.log(news);
-
   return (
     <section className="inner-container mx-auto px-0 grid lg:grid-cols-2 grid-cols-1 xl:gap-12 gap-8 lg:mt-24 mt-12 pb-12">
       {news.map(({ img, title, desc, author, date, view }: NewsType) => (
