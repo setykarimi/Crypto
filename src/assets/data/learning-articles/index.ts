2d4b9fe79db5082a3f66414f9d299654f585e555
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-05 21:27:11 +0330

    Subject: Learning articles, Description: Create new folder for learnong articles static data, Date: 11/05/2023

---
+++ src/assets/data/learning-articles/index.ts
@@ -0,0 +1,41 @@
+import { img } from "@data";
+import { ArticlesInitType } from "./initialType";
+
+export const learning_articles: ArticlesInitType = [
+  {
+    title: "معامله و ترید",
+    img: img.article1_img,
+    author: 250,
+    view: 5241,
+  },
+  {
+    title: "کیف پول (والت)",
+    img: img.article2_img,
+    author: 324,
+    view: 4567,
+  },
+  {
+    title: "مفاهیم پایه",
+    img: img.article3_img,
+    author: 134,
+    view: 6789,
+  },
+  {
+    title: "متاورس",
+    img: img.article4_img,
+    author: 189,
+    view: 1236,
+  },
+  {
+    title: "سرمایه‌گذاری",
+    img: img.article5_img,
+    author: 126,
+    view: 3467,
+  },
+  {
+    title: "آموزش",
+    img: img.article6_img,
+    author: 276,
+    view: 4897,
+  },
+];
---
+++ src/assets/data/learning-articles/initialType.ts
@@ -0,0 +1,6 @@
+export type ArticlesInitType = {
+  title: string;
+  img: string;
+  author: number;
+  view: number;
+}[];
