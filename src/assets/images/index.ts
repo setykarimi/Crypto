a62fa7982ba0819ca40c6fe112bd31d2b7eef89b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-06 21:31:42 +0330

    Subject: Images, Description: Add image webp, Date: 11/06/2023

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,12 +1,12 @@
 // @@@__________ news __________@@@
-import news1_img from "@assets/images/news/news1.png";
-import news2_img from "@assets/images/news/news2.png";
-import news3_img from "@assets/images/news/news3.png";
-import news4_img from "@assets/images/news/news4.png";
-import news5_img from "@assets/images/news/news5.png";
-import news6_img from "@assets/images/news/news6.png";
-import news7_img from "@assets/images/news/news7.png";
-import news8_img from "@assets/images/news/news8.png";
+import news1_img from "@assets/images/news/news1.webp";
+import news2_img from "@assets/images/news/news2.webp";
+import news3_img from "@assets/images/news/news3.webp";
+import news4_img from "@assets/images/news/news4.webp";
+import news5_img from "@assets/images/news/news5.webp";
+import news6_img from "@assets/images/news/news6.webp";
+import news7_img from "@assets/images/news/news7.webp";
+import news8_img from "@assets/images/news/news8.webp";
 
 // @@@__________ logos __________@@@
 import logo_red from "@assets/images/logos/logoRed.svg";
@@ -15,35 +15,35 @@ import logo_gray from "@assets/images/logos/logoGray.svg";
 import logo_white from "@assets/images/logos/logoWhite.svg";
 
 // @@@__________ backgrounds __________@@@
-import footer_bg from "@assets/images/backgrounds/footerBg.svg";
-import crypto_market_bg from "@assets/images/backgrounds/cryptoMarketBg.svg";
-import online_prices_bg from "@assets/images/backgrounds/onlinePricesBg.png";
-import banner_home_bg from "@assets/images/banner/bannerHomeBg.svg";
-import online_calculator_bg from "@assets/images/backgrounds/onlineCalculatorBg.svg";
-import article_card_bg from "@assets/images/backgrounds/articleCardBg.svg";
+import footer_bg from "@assets/images/backgrounds/footerBg.webp";
+import crypto_market_bg from "@assets/images/backgrounds/cryptoMarketBg.webp";
+import online_prices_bg from "@assets/images/backgrounds/onlinePricesBg.webp";
+import banner_home_bg from "@assets/images/backgrounds/bannerHomeBg.webp";
+import online_calculator_bg from "@assets/images/backgrounds/onlineCalculatorBg.webp";
+import article_card_bg from "@assets/images/backgrounds/articleCardBg.webp";
 
 // @@@__________ blog __________@@@
 import blog1_img from "@assets/images/blog/blog1.png";
 
 // @@@__________ items __________@@@
-import banner_home_wallet from "@assets/images/banner/bannerHomeWallet.svg";
-import credit_card from "@assets/images/items/creditCard.svg";
-import wallet from "@assets/images/items/wallet.svg";
-import digital_wallet_title from "@assets/images/items/digitalWalletTitle.svg";
-import new_banner_img from "@assets/images/items/newsBannerItem.svg";
-import learning_articles_banner from "@assets/images/items/learningArticlesBanner.svg";
+import banner_home_wallet from "@assets/images/items/bannerHomeWallet.webp";
+import credit_card from "@assets/images/items/creditCard.webp";
+import wallet from "@assets/images/items/wallet.webp";
+import digital_wallet_title from "@assets/images/items/digitalWalletTitle.webp";
+import new_banner_img from "@assets/images/items/newsBannerItem.webp";
+import learning_articles_banner from "@assets/images/items/learningArticlesBanner.webp";
 
 // @@@__________ icons __________@@@
-import arrow_down_blue from "@assets/images/icon/arrowDownBlue.svg";
-import arrow_down from "@assets/images/icon/arrowDown.svg";
+import arrow_down_blue from "@assets/images/icon/arrowDownBlue.webp";
+import arrow_down from "@assets/images/icon/arrowDown.webp";
 
 // @@@__________ articles __________@@@
-import article1_img from "@assets/images/learningArticles/article1.svg";
-import article2_img from "@assets/images/learningArticles/article2.svg";
-import article3_img from "@assets/images/learningArticles/article3.svg";
-import article4_img from "@assets/images/learningArticles/article4.svg";
-import article5_img from "@assets/images/learningArticles/article5.svg";
-import article6_img from "@assets/images/learningArticles/article6.svg";
+import article1_img from "@assets/images/learningArticles/article1.webp";
+import article2_img from "@assets/images/learningArticles/article2.webp";
+import article3_img from "@assets/images/learningArticles/article3.webp";
+import article4_img from "@assets/images/learningArticles/article4.webp";
+import article5_img from "@assets/images/learningArticles/article5.webp";
+import article6_img from "@assets/images/learningArticles/article6.webp";
 
 export {
   news1_img,
