6cf7e8e0b8c16c02335957afe1b336d05ce6cb49
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-07 18:37:26 +0330

    Subject: OnlineTable, Description: Add components, Date: 11/07/2023

--- src/pages/onlinePrices.tsx
+++ src/pages/onlinePrices.tsx
@@ -1,3 +1,13 @@
+import { OnlinePricesTable } from "@common/table/tableOnlinePrices";
+import CryptoMarketCards from "@common/cards/cardCryptoMarket";
+
 export default function OnlinePricesPage() {
-  return <div>OnlinePricesPage</div>;
+  return (
+    <>
+      <CryptoMarketCards />
+      <section className="inner-container mx-auto md:px-0 px-2">
+        <OnlinePricesTable />
+      </section>
+    </>
+  );
 }
