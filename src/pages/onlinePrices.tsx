eeaf733ea10db29ba9857d8913e2dfd68d10c663
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-08 21:51:52 +0330

    Subject: OnlineTable, Description: Edit page content, Date: 11/08/2023

--- src/pages/onlinePrices.tsx
+++ src/pages/onlinePrices.tsx
@@ -1,13 +1,34 @@
-import { OnlinePricesTable } from "@common/table/tableOnlinePrices";
+import { OnlinePricesTable } from "@common/table/onlinePrices";
 import CryptoMarketCards from "@common/cards/cardCryptoMarket";
+import { useState } from "react";
+import SafeExchange from "@pagesComp/home/content/safeExchange";
 
 export default function OnlinePricesPage() {
+  const [limit, setLimit] = useState<number>(10);
+  const clickHandler = () => {
+    setLimit((prv) => prv + 10);
+  };
   return (
     <>
       <CryptoMarketCards />
-      <section className="inner-container mx-auto md:px-0 px-2">
-        <OnlinePricesTable />
+      <h2 className="text-center lg:mt-24 mt-12 mb-8">
+        <span className="block font-black text-5xl text-red-primary">
+          آنلاین
+        </span>
+        <span className="font-bold text-2xl ml-24 -mt-3 block">قیمت‌های</span>
+      </h2>
+      <section className="inner-container mx-auto md:px-0 px-2 mb-12 flex flex-col">
+        <div className="shadow-lg bg-white rounded-3xl">
+          <OnlinePricesTable limit={limit} />
+        </div>
+        <button
+          onClick={clickHandler}
+          className="mx-auto mt-12 border-2 border-blue-primary text-blue-primary text-sm px-4 py-2 rounded-lg font-bold"
+        >
+          مشاهده رمز ارز‌های بیشتر
+        </button>
       </section>
+      <SafeExchange />
     </>
   );
 }
