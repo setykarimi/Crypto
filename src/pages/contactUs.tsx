f62e40011173af71fc84188fbf4cc6cf3bcf7c5a
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-10 19:31:06 +0330

    Subject: Contact us, Description: Add aboutUs component, Date: 11/10/2023

--- src/pages/contactUs.tsx
+++ src/pages/contactUs.tsx
@@ -1,9 +1,11 @@
+import AboutUs from "@pagesComp/contactUs/aboutUs";
 import ContactUsBanner from "@pagesComp/contactUs/banner";
 
 export default function ContactUsPage() {
   return (
     <>
       <ContactUsBanner />
+      <AboutUs />
     </>
   );
 }
