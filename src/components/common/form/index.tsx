2243d2d7a0abe81d57229698b3a30f01faf8ae86
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-11 22:42:00 +0330

    Subject: Forms, Description: edit for react hook form, Date: 11/11/2023

--- src/components/common/form/index.tsx
+++ src/components/common/form/index.tsx
@@ -1,5 +1,49 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 
 export default function ContactUsForm() {
-  return <div>Form</div>;
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+
+  type FormValues = {
+    username: string;
+    email: string;
+  };
+
+  const onSubmit = (data: FormValues) => {
+    console.log("formSUbmit", data);
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)} noValidate>
+        <div>
+          <label htmlFor="username">نام و نام خانوادگی</label>
+          <input
+            type="text"
+            id="username"
+            {...register("username", { required: {
+                value: true,
+                message: "Username is required"
+            } })}
+          />
+
+          <label htmlFor="email">ایمیل</label>
+          <input
+            type="email"
+            id="email"
+            {...register("email", {
+              pattern: {
+                value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                message: "Invalid email format",
+              },
+            })}
+          />
+
+          <button type="submit">ارسال نظر</button>
+        </div>
+      </form>
+      <DevTool control={control} />
+    </div>
+  );
 }
