494cbfe5e7f8e332578c3697a672864bc19b5195
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-22 22:30:58 +0330

    Subject: Digital curruncy, Description: edit content, Date: 11/22/2023

--- src/components/pages/buy-sell-digital-currency/cards/wallet-status/index.tsx
+++ src/components/pages/buy-sell-digital-currency/cards/wallet-status/index.tsx
@@ -4,7 +4,7 @@ import MyWalletStatusContent from "./components/content";
 export default function WalletStatus() {
   return (
     <div
-      className="bg-white p-4 rounded-2xl grid grid-cols-2 gap-4"
+      className="bg-white p-4 rounded-2xl grid xl:grid-cols-2 gap-4"
       style={{
         boxShadow: "0px 12px 25px -9px rgba(83, 83, 83, 0.15)",
       }}
