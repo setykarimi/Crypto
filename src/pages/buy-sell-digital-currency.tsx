21511f582ab1b54552728b33c84a5a5e815be663
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-25 22:05:58 +0330

    Subject: Digital curruncy, Description: Edit content and style, Date: 11/25/2023

--- src/pages/buy-sell-digital-currency.tsx
+++ src/pages/buy-sell-digital-currency.tsx
@@ -1,11 +1,13 @@
 import WalletStatus from "@pagesComp/buy-sell-digital-currency/cards/wallet-status";
 import MyInfoCard from "@pagesComp/buy-sell-digital-currency/cards/my-info";
+import Calculator from "@common/calculator";
 
 export default function BuySellDigitalCurrency() {
   return (
     <div className="inner-container mx-auto mt-8 grid lg:grid-cols-4 grid-cols-1 gap-4">
-      <div>
+      <div className="lg:flex md:grid grid-cols-2 flex-col gap-4">
         <MyInfoCard />
+        <Calculator customClassName="bg-white p-4 rounded-2xl card-shadow w-full relative flex flex-col"/>
       </div>
       <div className="lg:col-span-3 col-span-1">
         <WalletStatus />
