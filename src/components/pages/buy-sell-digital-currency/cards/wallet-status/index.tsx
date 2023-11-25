3a1dda773afbce94f91a735bec7dfcd182d39071
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-25 22:05:14 +0330

    Subject: Digital curruncy, Description: Edti content and style, Date: 11/24/2023

--- src/components/pages/buy-sell-digital-currency/cards/wallet-status/index.tsx
+++ src/components/pages/buy-sell-digital-currency/cards/wallet-status/index.tsx
@@ -3,12 +3,7 @@ import MyWalletStatusContent from "./components/content";
 
 export default function WalletStatus() {
   return (
-    <div
-      className="bg-white p-4 rounded-2xl grid xl:grid-cols-2 gap-4"
-      style={{
-        boxShadow: "0px 12px 25px -9px rgba(83, 83, 83, 0.15)",
-      }}
-    >
+    <div className="bg-white p-4 rounded-2xl grid xl:grid-cols-2 gap-4 card-shadow">
       <MyWalletStatusContent />
       <MyWalletStatusChart />
     </div>
