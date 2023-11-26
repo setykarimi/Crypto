9d67281d5681d7087ab72373eacaf397bb621d8b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-26 22:00:01 +0330

    Subject: Digital curruncy, Description: Add functions and data, Date: 11/26/2023

--- src/components/common/calculator/components/convert-to.tsx
+++ src/components/common/calculator/components/convert-to.tsx
@@ -1,13 +1,22 @@
+import { useExchangeStore } from "@store/exchange";
+
 export default function ConvertTo({ data }: any) {
+  const setTo = useExchangeStore((state) => state.setTo);
   return (
     <div>
       <label className="text-blue-primary font-semiBold mb-2 text-sm block">
         تبدیل به
       </label>
       <div className="bg-blue-200 py-1 px-4 rounded-lg flex gap-4">
-        <select className="w-full py-1 bg-transparent outline-none">
+        <select
+          className="w-full py-1 bg-transparent outline-none"
+          onChange={(e) => setTo(e.target.value)}
+        >
+          <option hidden selected className="text-left m-4">انتخاب کنید</option>
           {data?.map(({ name }: any) => (
-            <option className="text-left m-4">{name}</option>
+            <option className="text-left m-4" value={name}>
+              {name}
+            </option>
           ))}
         </select>
       </div>
