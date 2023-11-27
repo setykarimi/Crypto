8a00fcb3127a44c68a2e17d5cc6ac77dfe9c75cd
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-27 22:21:36 +0330

    Subject: Digital curruncy, Description: Show result, Date: 11/27/2023

--- src/components/common/calculator/components/result.tsx
+++ src/components/common/calculator/components/result.tsx
@@ -1,11 +1,12 @@
-import { HiOutlineSwitchHorizontal } from 'react-icons/hi'
+import { useExchangeStore } from "@store/exchange";
 
 export default function Result() {
-  return (
+  const result = useExchangeStore((state) => state.result);
+  return result ? (
     <div className="flex gap-2 justify-center text-sm mt-2 text-blue-primary">
-    <span>۰.۰۰۱۰۲۰۴۲۱ تتر</span>
-    <HiOutlineSwitchHorizontal />
-    <span>۱ تومان</span>
-  </div>
-  )
+      <span>{result} تتر</span>
+    </div>
+  ) : (
+    <></>
+  );
 }
