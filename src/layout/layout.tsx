418fb376dca975fc2429b3bf828a2e17d4901bbb
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-27 22:20:16 +0330

    Subject: Digital curruncy, Description: Add ToastContainer, Date: 11/27/2023

--- src/layout/layout.tsx
+++ src/layout/layout.tsx
@@ -1,22 +1,26 @@
-import { Outlet, useLocation } from "react-router-dom";
 import { useEffect } from "react";
+import { Toaster } from "react-hot-toast";
+import { Outlet, useLocation } from "react-router-dom";
 import Footer from "./footer";
 import Navbar from "./navbar";
 
 const Layout = () => {
-    const { pathname } = useLocation();
-    useEffect(() => {
-        window.scrollTo({top: 0, behavior: 'smooth'});
-      }, [pathname]);
-    return ( 
-        <div className="container">
-            <Navbar />
-            <main>
-                <Outlet />
-            </main>
-            <Footer />
-        </div>
-     );
-}
- 
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, [pathname]);
+
+  return (
+    <div className="container">
+      <Toaster position="top-left" reverseOrder={false} />
+      <Navbar />
+      <main>
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
+
 export default Layout;
