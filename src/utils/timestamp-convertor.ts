7748dedee6fd8ea6ea9b7b4cf30a57dcc546cb8d
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-11-28 22:14:19 +0330

    Subject: Digital curruncy, Description: Create new function for convert timestamp, Date: 11/28/2023

---
+++ src/utils/timestamp-convertor.ts
@@ -0,0 +1,7 @@
+export function timestampConvertor(timestamp: number) {
+  return new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+  }).format(timestamp);
+}
