c30d747896fc3167c907dc4833b2974e02aa4690
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-03 22:56:57 +0330

    Subject: Digital curruncy, Description: Add transaction component, Date: 12/3/2023

--- src/pages/buy-sell-digital-currency.tsx
+++ src/pages/buy-sell-digital-currency.tsx
@@ -1,3 +1,4 @@
+import Transactions from "@pagesComp/buy-sell-digital-currency/transactions";
 import Calculator from "@common/calculator";
 import LiveDemo from "@pagesComp/buy-sell-digital-currency/cards/live-demo";
 import MyInfoCard from "@pagesComp/buy-sell-digital-currency/cards/my-info";
@@ -7,9 +8,10 @@ import WholeChart from "@pagesComp/buy-sell-digital-currency/chart";
 export default function BuySellDigitalCurrency() {
   return (
     <div className="inner-container mx-auto mt-8 grid lg:grid-cols-4 grid-cols-1 gap-4">
-      <div className="lg:flex md:grid grid-cols-2 flex flex-col gap-4">
+      <div className="flex flex-col gap-4">
         <MyInfoCard />
         <Calculator customClassName="bg-white p-4 rounded-2xl card-shadow w-full relative flex flex-col"/>
+        <Transactions />
       </div>
       <div className="lg:col-span-3 col-span-1">
         <WalletStatus />
