053d975598d869cc5b74ab3f8d38d3e8f80faf63
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-04 22:18:40 +0330

    Subject: Loadings, Description: Add skeleton, Date: 12/4/2023

--- src/components/pages/buy-sell-digital-currency/chart/index.tsx
+++ src/components/pages/buy-sell-digital-currency/chart/index.tsx
@@ -2,20 +2,23 @@ import { useCoins } from "@hooks/useCoins";
 import LinearChart from "./linear-chart";
 
 export default function WholeChart() {
-  const { data: bitcoin, isLoading } = useCoins(`/coins/bitcoin
-    `);
-    console.log(bitcoin);
-    if(isLoading){
-        return <></>
-    }
+  const { data: bitcoin, isLoading } = useCoins(`/coins/bitcoin`);
+
+  if (isLoading) {
+    return (
+      <div className="bg-white animate-pulse card-shadow p-4 rounded-xl mt-4">
+        <div className="rounded-full bg-slate-200 h-14 w-14"></div>
+      </div>
+    );
+  }
   return (
     <div className="bg-white card-shadow p-4 rounded-xl mt-4">
-        <div>
-            <div className="flex items-center gap-2">
-                <img src={bitcoin.icon} alt={bitcoin.symbol} className="w-12 h-12"/>
-                <span className="font-black text-2xl ">{bitcoin.symbol}</span>
-            </div>
+      <div>
+        <div className="flex items-center gap-2">
+          <img src={bitcoin.icon} alt={bitcoin.symbol} className="w-12 h-12" />
+          <span className="font-black text-2xl ">{bitcoin.symbol}</span>
         </div>
+      </div>
       <LinearChart />
     </div>
   );
