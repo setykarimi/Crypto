ad3cabd994cff3045870b610366128e3fa9a7790
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-04 22:18:22 +0330

    Subject: Loadings, Description: Create new component for skeleton, Date: 12/4/2023

---
+++ src/components/pages/buy-sell-digital-currency/chart/linear-chart/skeleton.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function LinearChartSkeleton() {
+  return (
+    <div className="animate-pulse flex flex-col">
+    <div className="rounded bg-slate-200 h-40 mt-16 w-9/12 mr-auto ml-16 mb-16"></div>
+  </div>
+  )
+}
