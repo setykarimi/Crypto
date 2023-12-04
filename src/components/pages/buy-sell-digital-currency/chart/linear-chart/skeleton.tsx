47151e0e4d59d1fb61b8ce2a895624e2522bbd2c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-04 22:19:39 +0330

    Subject: Loadings, Description: Remove react import, Date: 12/4/2023

--- src/components/pages/buy-sell-digital-currency/chart/linear-chart/skeleton.tsx
+++ src/components/pages/buy-sell-digital-currency/chart/linear-chart/skeleton.tsx
@@ -1,9 +1,7 @@
-import React from 'react'
-
 export default function LinearChartSkeleton() {
   return (
     <div className="animate-pulse flex flex-col">
-    <div className="rounded bg-slate-200 h-40 mt-16 w-9/12 mr-auto ml-16 mb-16"></div>
-  </div>
-  )
+      <div className="rounded bg-slate-200 h-40 mt-16 w-9/12 mr-auto ml-16 mb-16"></div>
+    </div>
+  );
 }
