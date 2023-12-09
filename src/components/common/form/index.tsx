504230ff0f2ac1c6c420d94e5483037ea3a37ab2
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-09 16:33:38 +0330

    Subject: Folder Contant us, Description: Edit form content, Date: 12/9/2023

--- src/components/common/form/index.tsx
+++ src/components/common/form/index.tsx
@@ -1,109 +1,85 @@
-import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
+import { useForm } from "react-hook-form";
+import CustomInput from "./input";
+import CustomTextArea from "./text-area";
+import { FormValuesType } from "./type";
+
 
 export default function ContactUsForm() {
-  type FormValues = {
-    username: string;
-    email: string;
-    social: {
-      twitter: string;
-      facebook: string;
-    };
-    phoneNumbers: string[];
-  };
-  const form = useForm<FormValues>({
-    defaultValues: async () => {
-      const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users/1"
-      );
-      const data = await response.json();
-      return {
-        username: "Batman",
-        email: data.email,
-        social: {
-          twitter: "",
-          facebook: "",
-        },
-        phoneNumbers: ["", ""],
-      };
-    },
-  });
+  const form = useForm<FormValuesType>();
+
   const { register, control, handleSubmit, formState } = form;
+
   const { errors } = formState;
 
-  const onSubmit = (data: FormValues) => {
+  const onSubmit = (data: FormValuesType) => {
     console.log("formSUbmit", data);
   };
 
   return (
-    <div className="flex justify-end">
+    <div className="inner-container mx-auto mt-12 bg-white rounded-2xl card-shadow p-8">
+      <h4 className="text-center mb-4 font-bold text-gray-800 text-lg">
+        خوشحال میشم نظرات و پیشنهاداتتون رو برام بنویسید تا بتونم تو پروژه‌های
+        بعدیم ازشون استفاده کنم.
+      </h4>
       <form onSubmit={handleSubmit(onSubmit)} noValidate>
-        <div>
-          <div>
-            <label htmlFor="username">نام و نام خانوادگی</label>
-            <input
-              type="text"
-              id="username"
-              {...register("username", {
-                required: {
-                  value: true,
-                  message: "Username is required",
+        <div className="flex flex-col gap-2">
+          <CustomInput
+            register={register}
+            errors={errors}
+            label="نام و نام خانوادگی"
+            name="username"
+          />
+          <CustomInput
+            register={register}
+            errors={errors}
+            label="موضوع"
+            name="subject"
+          />
+          <CustomInput
+            register={register}
+            errors={errors}
+            label="ایمیل"
+            name="email"
+            type="email"
+            validationFormat={{
+              required: {
+                value: true,
+                message: "ایمیل الزامی است.",
+              },
+              pattern: {
+                value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
+                message: "ایمیل نادرست است.",
+              },
+              validate: {
+                notAdmin: (fieldValue: string) => {
+                  return (
+                    fieldValue !== "admin@example.com" ||
+                    "لطفا ایمیل دیگری وارد نمایید."
+                  );
                 },
-              })}
-            />
-            <p>{errors.username?.message}</p>
-          </div>
-
-          <div>
-            <label htmlFor="email">ایمیل</label>
-            <input
-              type="email"
-              id="email"
-              {...register("email", {
-                pattern: {
-                  value: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
-                  message: "Invalid email format",
+                notBlackListed: (fieldValue: string) => {
+                  return (
+                    !fieldValue.endsWith("badddmain.com") ||
+                    "دامنه وارد شده صحیح نیست."
+                  );
                 },
-                validate: {
-                  notAdmin: (fieldValue) => {
-                    return (
-                      fieldValue !== "admin@example.com" ||
-                      "Enter a diffrent email address"
-                    );
-                  },
-                  notBlackListed: (fieldValue) => {
-                    return (
-                      !fieldValue.endsWith("badddmain.com") ||
-                      "This domain is not supported"
-                    );
-                  },
-                },
-              })}
-            />
-            <p>{errors.email?.message}</p>
-          </div>
+              },
+            }}
+          />
+          <CustomTextArea
+            register={register}
+            errors={errors}
+            label="توضیحات"
+            name="description"
+          />
 
-          <div>
-            <label htmlFor="twitter">Twitter</label>
-            <input type="text" id="twitter" {...register("social.twitter")} />
-          </div>
-
-          <div>
-            <label htmlFor="facebook">Faceook</label>
-            <input type="text" id="facebook" {...register("social.facebook")} />
-          </div>
-
-          <div>
-            <label htmlFor="primary-phone">primary phone</label>
-            <input type="text" id="primary-phone" {...register("phoneNumbers.0")} />
-          </div>
-          <div>
-            <label htmlFor="secondary-phone">secondary phone</label>
-            <input type="text" id="secondary-phone" {...register("phoneNumbers.1")} />
-          </div>
-
-
-          <button type="submit">ارسال نظر</button>
+          <button
+            type="submit"
+            className="bg-blue-primary text-white px-16 mx-auto py-2 rounded-lg "
+          >
+            ارسال نظر
+          </button>
         </div>
       </form>
       <DevTool control={control} />
