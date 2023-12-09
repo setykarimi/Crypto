f73b17c91d1903806fd1c0317c9793a7541e0abc
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-09 11:43:20 +0330

    Subject: Folder structure, Description: Rename file, Date: 12/9/2023

--- src/assets/data/index.ts
+++ src/assets/data/index.ts
@@ -1,4 +1,4 @@
-export * as news_data from './news/news'
+export * as news_data from './news'
 export * as news_init_type from './news/type'
 export *  from './learning-articles'
 export *  from './transactions'
