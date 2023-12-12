123287363c218036c5001bcc8f669fa68883e03c
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:39:27 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/pages/home/banner/banner-home.tsx
+++ src/components/pages/home/banner/banner-home.tsx
@@ -3,12 +3,19 @@ import { img } from "@data";
 export default function HomeBanner() {
   return (
     <div
-      className="-mt-[5.6rem] z-0 relative xl:pb-28 pb-16 lg:px-0 px-4"
-      style={{ background: `url(${img.banner_home_bg}) no-repeat center bottom`, backgroundSize: 'cover' }}
+      className="-mt-[5.6rem] z-0 relative xl:pb-28 pb-16 lg:px-0 px-4 rounded-2xl"
+      style={{
+        background: `url(${img.banner_home_bg}) no-repeat center bottom`,
+        backgroundSize: "cover",
+      }}
     >
       <div className="inner-container mx-auto flex md:flex-row flex-col-reverse md:justify-between items-center md:px-0 px-2">
         <ContentTitle />
-        <img src={img.banner_home_wallet} alt="wallet" className="xl:w-fit lg:w-96 md:w-80 w-56 mr-auto"/>
+        <img
+          src={img.banner_home_wallet}
+          alt="wallet"
+          className="xl:w-fit lg:w-96 md:w-80 w-56 mr-auto"
+        />
       </div>
       <button className="absolute bottom-0 mx-auto left-0 right-0 w-fit px-8 py-2">
         <span className="xl:h-8 h-4 bg-gray-400 w-[2px] rounded-full block"></span>
@@ -36,9 +43,12 @@ const ContentTitle = () => {
       <span className="font-light block text-white mt-1 md:text-base text-sm">
         مطمئن‌ترین ارز دیجیتال در ایران
       </span>
-      <button className="w-10/12 md:text-base text-sm border-2 border-yellow-primary py-2 rounded-xl text-yellow-primary font-extraBold mt-8">
+      <a
+        href="#crypter-market"
+        className="block px-4 w-fit  md:text-base text-sm border-2 border-yellow-primary py-2 rounded-xl text-yellow-primary font-extraBold mt-8"
+      >
         بزن بریم برای دریافت جایزه
-      </button>
+      </a>
     </div>
   );
 };
