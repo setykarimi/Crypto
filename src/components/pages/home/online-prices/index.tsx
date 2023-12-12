30d22da22a18567b0e09a3381fec34d523df2a41
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:38:33 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/pages/home/online-prices/index.tsx
+++ src/components/pages/home/online-prices/index.tsx
@@ -1,19 +1,12 @@
 import { OnlinePricesTable } from "@common/table/online-prices";
 import SectionsTitle from "@common/titles/section";
-import { img } from "@data";
 import { BsArrowLeft } from "react-icons/bs";
 import { Link } from "react-router-dom";
 
 export default function OnlinePrices() {
   return (
-    <div
-      className="lg:mt-24 mt-12 pb-12 md:pt-24 pt-8 md:w-full w-[95%] mx-auto md:rounded-none rounded-3xl"
-      style={{
-        background: `url(${img.online_prices_bg}) no-repeat center top`,
-        backgroundSize: "cover",
-      }}
-    >
-      <section className="inner-container mx-auto px-0">
+    <div className="mt-12 pb-12 md:pt-24 px-8 md:w-full w-[95%] mx-auto md:rounded-none rounded-3xl">
+      <section>
         {/* Title Section*/}
         <SectionsTitle number={2} title="آنلاین" subTitle="قیمت‌های">
           <button className="border border-blue-primary text-blue-primary rounded-lg flex items-center gap-2 p-2 h-fit md:text-sm text-xs font-semiBold">
