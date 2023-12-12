058e5c9200815cdcd321942643f000c3751cb16b
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:40:49 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/common/blogs/blog-card/index.tsx
+++ src/components/common/blogs/blog-card/index.tsx
@@ -23,13 +23,13 @@ export default function BlogCard() {
             <span className="block font-extraBold text-red-primary md:text-xl text-base mb-2">
               آموزش نوسان‌گیری در کریپتر
             </span>
-            <p className="text-gray-400 font-semiBold text-justify md:text-base text-sm">
+            <p className="text-gray-300 text-justify leading-relaxed">
               امروزه بازار ارز دیجیتال از جذاب‌ترین بازارها برای سرمایه‌گذاری
               است. برخی افراد در این بازار سودهای زیادی را ....
             </p>
           </div>
         </div>
-        <div className="flex md:gap-12 items-center md:mr-auto md:-mt-6 mt-2 md:justify-end justify-between">
+        <div className="flex md:gap-10 items-center md:mr-auto md:-mt-6 mt-2 md:justify-end justify-between">
           <div className="flex gap-2 items-center">
             <FaPen className="text-gray-400" size={16} />
             <span className="text-sm text-gray-400 font-semiBold">
