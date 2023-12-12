c3700648036d7c8f80353bc781ddfbe266252ac0
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:39:55 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/pages/buy-sell-digital-currency/cards/wallet-status/components/chart/index.tsx
+++ src/components/pages/buy-sell-digital-currency/cards/wallet-status/components/chart/index.tsx
@@ -8,7 +8,7 @@ export default function MyWalletStatusChart() {
   return isLoading ? (
     <LoadingPieChart />
   ) : (
-    <div className="grid md:grid-cols-3 grid-cols-2 gap-2 md:pt-0 pt-4 md:border-t-0 border-t border-dashed border-t-gray-200">
+    <div className="grid md:grid-cols-3 grid-cols-2 gap-2 md:pt-3 pt-4 md:border-t-0 border-t border-dashed border-t-gray-200">
       <PieChart data={coins?.result[0]} />
       <PieChart data={coins?.result[1]} />
       <PieChart data={coins?.result[2]} />
