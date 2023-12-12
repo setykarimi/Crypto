76c2d6a3efb807d7f5bfc82e95cad36aaa52b4eb
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:39:20 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/pages/home/calculator/onlineCalculator.tsx
+++ src/components/pages/home/calculator/onlineCalculator.tsx
@@ -5,13 +5,13 @@ import Calculator from "@common/calculator";
 export default function OnlineCalculator() {
   return (
     <div
-      className="lg:mt-24 mt-12 pb-12"
+      className="lg:mt-24 mt-12 pb-12 px-8"
       style={{
         background: `url(${img.online_calculator_bg}) no-repeat center left`,
         backgroundSize: "contain",
       }}
     >
-      <section className="inner-container mx-auto md:px-0 px-2">
+      <section>
         <SectionsTitle number={4} title="مبادله" subTitle="ماشین حساب آنلاین" />
         <div className="grid md:grid-cols-2 grid-cols-1 lg:gap-16 gap-4">
           <img
