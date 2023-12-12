8c40a26d319edb598e7c30d80f5897a9ea74ccb8
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:40:35 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/common/calculator/components/convert-to.tsx
+++ src/components/common/calculator/components/convert-to.tsx
@@ -14,7 +14,7 @@ export default function ConvertTo() {
       <label className="text-blue-primary font-semiBold mb-2 text-sm block">
         تبدیل به
       </label>
-      <div className="bg-blue-200 py-1 px-4 rounded-lg flex gap-4">
+      <div className="bg-white border border-gray-200 py-1 px-4 rounded-lg flex gap-4">
         <select
           className="w-full py-1 bg-transparent outline-none"
           onChange={(e) => setTo(e.target.value)}
