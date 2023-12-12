efc78a5b083f1b5c90d643b790beae01a39e9306
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:40:29 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/common/calculator/components/payment-amount.tsx
+++ src/components/common/calculator/components/payment-amount.tsx
@@ -18,7 +18,7 @@ export default function PaymentAmount() {
       <label className="text-blue-primary font-semiBold mb-2 text-sm block">
         مبلغ پرداختی
       </label>
-      <div className="bg-blue-200 py-1 px-4 rounded-lg flex gap-4">
+      <div className="bg-white border border-gray-200 py-1 px-4 rounded-lg flex gap-4">
         <input
           className="bg-transparent w-2/3 py-2 text-left outline-none placeholder:text-xs placeholder:text-gray-300 placeholder:text-right text-gray-400 text-sm ltr"
           placeholder="مبلغ مورد نظر خود را وارد کنید"
