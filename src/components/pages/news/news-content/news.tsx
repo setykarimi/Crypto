2dc9c97c8d0832054199db3079b9b43736a00221
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:38:20 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/pages/news/news-content/news.tsx
+++ src/components/pages/news/news-content/news.tsx
@@ -3,7 +3,7 @@ import { NewsType } from "./type";
 
 export default function NewsContent({ news }: any) {
   return (
-    <section className="inner-container mx-auto px-0 grid lg:grid-cols-2 grid-cols-1 xl:gap-12 gap-8 lg:mt-24 mt-12 pb-12">
+    <section className="px-0 grid lg:grid-cols-2 grid-cols-1 xl:gap-12 gap-8 lg:mt-24 mt-12 pb-12">
       {news.map(({ img, title, desc, author, date, view }: NewsType) => (
         <NewsCard
           img={img}
