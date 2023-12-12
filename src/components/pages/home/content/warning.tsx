c393d402da49211885e6d7c9b3067740cf087753
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:38:52 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/pages/home/content/warning.tsx
+++ src/components/pages/home/content/warning.tsx
@@ -1,7 +1,7 @@
 export default function Warning() {
   return (
-    <div className="bg-red-500 lg:mt-24 mt-12 ">
-      <section className="inner-container mx-auto md:px-0 px-2">
+    <div className="bg-red-500 lg:mt-24 mt-12 px-8 rounded-xl">
+      <section>
         <p className="text-justify text-lg text-white font-semiBold py-6 leading-loose">
           خواهشمند است در هنگام خرید و فروش ارزهای دیجیتال، به اندازه کافی
           مطالعه و دقت نموده و بر اساس دانش خود اقدام به مبادله نمایید. اکسچنج
