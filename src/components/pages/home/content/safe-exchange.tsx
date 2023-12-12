c4297cde51a554227607652a6404a6a95026b158
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-12 20:39:00 +0330

    Subject: Debug, Description: Styles edited, Date: 12/12/2023

--- src/components/pages/home/content/safe-exchange.tsx
+++ src/components/pages/home/content/safe-exchange.tsx
@@ -2,7 +2,7 @@ import { BsArrowLeft } from "react-icons/bs";
 
 export default function SafeExchange() {
   return (
-    <div className="bg-orange-primary lg:mt-24 mt-12 py-8 flex flex-col">
+    <div className="bg-orange-primary lg:mt-24 mt-12 p-8 rounded-xl flex flex-col">
       <h4 className="font-extraBold lg:text-4xl text-xl text-center text-white">
         با بهترین قیمت از امن‌ترین صرافی خرید کنید
       </h4>
