5a03cff23779601c85738661bf0953da5d8de8bf
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-17 16:53:44 +0330

    Subject: Debug, Description: Edit classNames for responsive view, Date: 12/17/2023

--- src/components/pages/learning-articles/banner/index.tsx
+++ src/components/pages/learning-articles/banner/index.tsx
@@ -2,7 +2,7 @@ import { img } from "@data";
 
 export default function BannerLearningArticles() {
   return (
-    <div className="bg-blue-50 -mt-[5.6rem] z-0 relative xl:pb-32 pb-20 lg:px-0 px-4 rounded-br-3xl rounded-bl-3xl">
+    <div className="bg-blue-50 header__banner">
       <div className="inner-container mx-auto flex md:flex-row flex-col-reverse md:justify-between items-center md:px-0 px-2">
         <img
           src={img.learning_articles_banner}
@@ -14,12 +14,12 @@ export default function BannerLearningArticles() {
         <h1 className="text-red-700 lg:font-bold font-black lg:text-7xl text-3xl">
           کـــــــــــریـپـتـر
         </h1>
-        <div className="flex items-center justify-center gap-16">
+        <div className="flex items-center justify-center gap-16 mt-2">
           <span className="font-semiBold lg:text-2xl text-xl text-blue-primary">
             بهترین مقالات را در
           </span>
           <span className="font-semiBold lg:text-2xl text-xl text-blue-primary">
-            بــــــــــــــــــــــــخوانــیـــــــــد
+            بــــــــــــــــــخوانــیــــد
           </span>
         </div>
       </div>
@@ -27,7 +27,7 @@ export default function BannerLearningArticles() {
         <img
           src={img.arrow_down_blue}
           alt="wallet"
-          className="lg:w-fit md:w-16 w-12"
+          className="md:w-16 w-12"
         />
       </button>
     </div>
