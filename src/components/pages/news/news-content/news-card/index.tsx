7e9e90cf5cda9e8ecb1bc3f093b7edfb7d114a1d
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-17 16:53:35 +0330

    Subject: Debug, Description: Edit classNames for responsive view, Date: 12/17/2023

--- src/components/pages/news/news-content/news-card/index.tsx
+++ src/components/pages/news/news-content/news-card/index.tsx
@@ -21,47 +21,53 @@ export default function NewsCard(props: PropsType) {
           "11px 11px 30px -5px rgba(6, 102, 151, 0.10), -6px -6px 26px 0px rgba(255, 255, 255, 0.03)",
       }}
     >
-      <div className="py-4 pr-4 md:pl-12 p-4 flex flex-col">
-        <div className="grid md:grid-cols-8 grid-cols-9 gap-4">
-          <div className="md:col-span-3 col-span-4">
+      <div className="p-4 flex flex-col">
+        <div className="md:grid grid-cols-8 flex flex-col gap-4">
+          <div className="md:col-span-3">
             <img
               src={img ?? img}
               alt={title}
               className="md:h-36 h-28 w-full object-cover rounded-xl "
             />
           </div>
-          <div className="col-span-5">
-            <span className="block font-extraBold text-red-primary md:text-xl text-base mb-2">
+
+          <div className="md:col-span-5 flex flex-col gap-2">
+            <span className="block font-extraBold text-red-primary md:text-xl text-base">
               {title}
             </span>
             <p className="text-gray-400 font-semiBold text-justify md:text-base text-sm">
               {desc}
             </p>
-          </div>
-        </div>
-        <div className="flex xl:gap-7 lg:gap-10 md:gap-7 items-center md:mr-auto md:-mt-6 mt-2 md:justify-end justify-between">
-          <div className="flex gap-1 items-center">
-            <FaPen
-              className="text-gray-400 xl:block lg:hidden block"
-              size={16}
-            />
-            <span className="text-sm text-gray-400 font-semiBold">
-              {author}
-            </span>
-          </div>
-          <div className="flex gap-1 items-center">
-            <BsCalendarDateFill
-              className="text-gray-400 xl:block lg:hidden block"
-              size={16}
-            />
-            <span className="text-sm text-gray-400 font-semiBold">{date}</span>
-          </div>
-          <div className="flex gap-1 items-center">
-            <HiEye
-              className="text-gray-400 xl:block lg:hidden block"
-              size={16}
-            />
-            <span className="text-sm text-gray-400 font-semiBold">{view}</span>
+
+            <div className="w-full flex gap-4 items-center justify-between mt-auto">
+              <div className="flex gap-1 items-center">
+                <FaPen
+                  className="text-gray-400 xl:block lg:hidden block"
+                  size={16}
+                />
+                <span className="text-sm text-gray-400 font-semiBold md:whitespace-normal whitespace-nowrap">
+                  {author}
+                </span>
+              </div>
+              <div className="flex gap-1 items-center">
+                <BsCalendarDateFill
+                  className="text-gray-400 xl:block lg:hidden block"
+                  size={16}
+                />
+                <span className="text-sm text-gray-400 font-semiBold">
+                  {date}
+                </span>
+              </div>
+              <div className="flex gap-1 items-center">
+                <HiEye
+                  className="text-gray-400 xl:block lg:hidden block"
+                  size={16}
+                />
+                <span className="text-sm text-gray-400 font-semiBold">
+                  {view}
+                </span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
