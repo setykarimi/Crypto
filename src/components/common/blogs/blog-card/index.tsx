aebc9db1ee0d4908c8521272071741b60e318c15
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-17 16:54:19 +0330

    Subject: Debug, Description: Edit classNames for responsive view, Date: 12/17/2023

--- src/components/common/blogs/blog-card/index.tsx
+++ src/components/common/blogs/blog-card/index.tsx
@@ -1,50 +1,51 @@
-import { FaPen } from "react-icons/fa";
-import { BsCalendarDateFill } from "react-icons/bs";
-import { HiEye } from "react-icons/hi";
 import { img } from "@data";
+import { BsCalendarDateFill } from "react-icons/bs";
+import { FaPen } from "react-icons/fa";
+import { HiEye } from "react-icons/hi";
 
 export default function BlogCard() {
   return (
     <div
-      className="bg-white rounded-xl flex relative"
+      className="bg-white rounded-xl flex relative md:w-full w-[30rem]"
       style={{
         boxShadow:
           "11px 11px 30px -5px rgba(6, 102, 151, 0.10), -6px -6px 26px 0px rgba(255, 255, 255, 0.03)",
       }}
     >
-      <div className="py-4 pr-4 md:pl-12 p-4 flex flex-col">
-        <div className="flex gap-4">
+      <div className="py-4 pr-4 xl:pl-8 p-4 flex flex-col">
+        <div className="flex md:flex-row flex-col gap-4">
           <img
             src={img.blog1_img}
             alt="blog"
-            className="md:w-36 w-24 md:h-36 h-34 object-cover rounded-xl"
+            className="md:w-36 w-full md:h-36 h-32 object-cover rounded-xl"
           />
-          <div>
-            <span className="block font-extraBold text-red-primary md:text-xl text-base mb-2">
+          <div className="flex flex-col gap-2">
+            <span className="block font-extraBold text-red-primary md:text-xl text-base">
               آموزش نوسان‌گیری در کریپتر
             </span>
             <p className="text-gray-300 text-justify leading-relaxed">
               امروزه بازار ارز دیجیتال از جذاب‌ترین بازارها برای سرمایه‌گذاری
               است. برخی افراد در این بازار سودهای زیادی را ....
             </p>
-          </div>
-        </div>
-        <div className="flex md:gap-10 items-center md:mr-auto md:-mt-6 mt-2 md:justify-end justify-between">
-          <div className="flex gap-2 items-center">
-            <FaPen className="text-gray-400" size={16} />
-            <span className="text-sm text-gray-400 font-semiBold">
-              سید حسین یعقوبی
-            </span>
-          </div>
-          <div className="flex gap-2 items-center">
-            <BsCalendarDateFill className="text-gray-400" size={16} />
-            <span className="text-sm text-gray-400 font-semiBold">
-              ۱۴۰۲/۰۱/۱۳
-            </span>
-          </div>
-          <div className="flex gap-2 items-center">
-            <HiEye className="text-gray-400" size={16} />
-            <span className="text-sm text-gray-400 font-semiBold">245</span>
+
+            <div className="flex gap-4 items-center justify-between mt-auto">
+              <div className="flex gap-2 items-center">
+                <FaPen className="text-gray-400" size={16} />
+                <span className="md:text-sm text-xs md:whitespace-normal whitespace-nowrap text-gray-400 font-semiBold">
+                  سید حسین یعقوبی
+                </span>
+              </div>
+              <div className="flex gap-2 items-center">
+                <BsCalendarDateFill className="text-gray-400" size={16} />
+                <span className="text-sm text-gray-400 font-semiBold">
+                  ۱۴۰۲/۰۱/۱۳
+                </span>
+              </div>
+              <div className="flex gap-2 items-center">
+                <HiEye className="text-gray-400" size={16} />
+                <span className="text-sm text-gray-400 font-semiBold">245</span>
+              </div>
+            </div>
           </div>
         </div>
       </div>
