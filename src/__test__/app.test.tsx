447179728552344672887beb07928ba61d277cfd
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-21 16:46:12 +0330

    Subject: UnitTest, Description: Edit test file, Date: 12/21/2023

--- src/__test__/app.test.tsx
+++ src/__test__/app.test.tsx
@@ -1,12 +1,17 @@
-import '@testing-library/jest-dom'
-import { render } from "@testing-library/react"
-import ContactUsPage from "../pages/contact-us"
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Accordion from "../components/common/accordion/index";
 
-test('demo', () => {
-    expect(true).toBe(true)
-})
+test("demo", () => {
+  expect(true).toBe(true);
+});
 
 test("Renders the main page", () => {
-    render(<ContactUsPage />)
-    expect(true).toBeTruthy()
-})
+  const props = {
+    content: "content Accordion",
+    title: "title",
+  };
+  render(<Accordion title={props.title} content={props.content} />);
+  expect(true).toBeTruthy();
+  expect(screen.getByText(props.content)).toBeInTheDocument();
+});
