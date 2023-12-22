f1ab0a0b6338b1c14db0a21e17b509789e35652e
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-22 13:04:52 +0330

    Subject: News detail, Description: Edit content, Date: 12/22/2023

--- src/components/pages/news/news-content/news.tsx
+++ src/components/pages/news/news-content/news.tsx
@@ -4,8 +4,9 @@ import { NewsType } from "./type";
 export default function NewsContent({ news }: any) {
   return (
     <section className="px-0 grid lg:grid-cols-2 grid-cols-1 xl:gap-12 gap-8 lg:mt-24 mt-12 pb-12">
-      {news.map(({ img, title, desc, author, date, view }: NewsType) => (
+      {news.map(({ img, title, desc, author, date, view, id }: NewsType) => (
         <NewsCard
+          id={id}
           img={img}
           title={title}
           desc={desc}
