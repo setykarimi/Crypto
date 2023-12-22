8b267ba57ecf22362b65fcc60f4bc529ff81633f
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-22 13:04:05 +0330

    Subject: News detail, Description: Add  new route, Date: 12/22/2023

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,12 @@
-import BuySellDigitalCurrency from "@pages/buy-sell-digital-currency";
+import NewsDetail from "@pages/news-datail[news-id]";
 import Layout from "@layout/layout";
+import BuySellDigitalCurrency from "@pages/buy-sell-digital-currency";
+import ContactUsPage from "@pages/contact-us";
 import HomePage from "@pages/home";
 import LearningArticlesPage from "@pages/learning-articles";
 import NewsPage from "@pages/news";
 import OnlinePricesPage from "@pages/online-prices";
 import { createBrowserRouter } from "react-router-dom";
-import ContactUsPage from "@pages/contact-us";
 
 export const routes = createBrowserRouter([
   {
@@ -35,6 +36,10 @@ export const routes = createBrowserRouter([
       {
         path: "/buy-sell/digital-currency",
         element: <BuySellDigitalCurrency />,
+      },
+      {
+        path: "/news/detail/:id",
+        element: <NewsDetail />,
       },
     ],
   },
