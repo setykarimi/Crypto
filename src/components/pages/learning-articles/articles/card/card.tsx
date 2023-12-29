73ad4b45240e1cbc5667897cafe0daa960625049
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-29 11:58:11 +0330

    Subject: Debug, Description: Edit style for resopnsive view, Date: 12/29/2023

--- src/components/pages/learning-articles/articles/card/card.tsx
+++ src/components/pages/learning-articles/articles/card/card.tsx
@@ -6,7 +6,7 @@ import { ArticleCardInitValue } from "./initial-value";
 
 export default function ArticleCard({ article }: ArticleCardInitValue) {
   return (
-    <div className="md:p-8 p-2 rounded-xl flex flex-col md:gap-6 gap-3 items-center"
+    <div className="md:p-4 p-2 rounded-xl flex flex-col md:gap-4 gap-3 items-center"
     style={{
       background: `url(${img.article_card_bg}) no-repeat center right`,
         backgroundSize: "cover",
@@ -14,26 +14,26 @@ export default function ArticleCard({ article }: ArticleCardInitValue) {
       <img
         src={article.img}
         alt="learning article"
-        className="rounded-xl"
+        className="rounded-lg"
       />
-      <span className="text-orange-primary font-semiBold md:text-xl text-lg block mx-auto text-center">
+      <span className="text-orange-primary font-bold md:text-xl text-lg block mx-auto text-center">
         {article.title}
       </span>
       <Link
-        to="/"
-        className="bg-blue-primary text-white md:text-lg py-2 rounded-lg block text-center w-full"
+        to=""
+        className="md:text-base text-sm bg-blue-primary text-white py-2 rounded-md block text-center w-full"
       >
         بزن بریم
       </Link>
 
-      <div className="flex justify-between md:gap-4 gap-2 items-center w-full">
+      <div className="flex justify-between md:gap-4 gap-2 items-center w-full md:px-4">
         <div className="text-gray-400 flex items-center">
           <PiPencilSimpleLineFill className="block ml-1" />
-          <span className="md:font-bold md:text-base text-xs">{article.author} مقاله</span>
+          <span className="md:font-semibold md:text-sm text-xs">{article.author} مقاله</span>
         </div>
         <div className="text-gray-400 flex items-center">
           <AiFillEye className="block ml-1" />
-          <span className="md:font-bold md:text-base text-xs">{article.view} بازدید</span>
+          <span className="md:font-semibold md:text-sm text-xs">{article.view} بازدید</span>
         </div>
       </div>
     </div>
