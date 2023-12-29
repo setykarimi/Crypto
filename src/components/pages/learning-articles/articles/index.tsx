dda6c8dff4a791fbf6e6c47bc62cebc27362fd31
Setayesh <75073451+setykarimi@users.noreply.github.com>
2023-12-29 11:58:07 +0330

    Subject: Debug, Description: Edit style for resopnsive view, Date: 12/29/2023

--- src/components/pages/learning-articles/articles/index.tsx
+++ src/components/pages/learning-articles/articles/index.tsx
@@ -12,7 +12,7 @@ export default function Articles() {
         </h1>
         {/* End Title Section*/}
 
-        <div className="grid lg:grid-cols-3 grid-cols-2 xl:gap-12 md:gap-8 gap-4 mt-12">
+        <div className="grid xl:grid-cols-4 lg:grid-cols-3 grid-cols-2 gap-4 mt-12">
           {static_data.learning_articles.map((article) => (
             <ArticleCard article={article} />
           ))}
